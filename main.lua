a654c66d114bbbc3fad84d061c930fe500403989
fdeijuhjfdhifdhijfdijohijfdijfdjoifdjff <108233386+fdeijuhjfdhifdhijfdijohijfdijfdjoifdjff@users.noreply.github.com>
2022-08-10 18:10:12 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,17 +1,27 @@
 local LocalPlayer = game:GetService("Players").LocalPlayer
-
 game.ReplicatedStorage.DefaultChatSystemChatEvents.OnMessageDoneFiltering.OnClientEvent:Connect(function(message)
-
     local player = message.FromSpeaker
     local chat = message.Message:lower()
-
-    if string.find(chat,"duy") then
-        game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("It's time for you to all realise....", "All")
+    if string.find(chat,"funnideath") then
+        game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("I AM UNDEFEATABLE!!", "All")
+        wait(6)
+        game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("wait.... i dont feel so good...", "All")
+        game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("🤢", "All")
         wait(5)
-        game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("I AM THE ONLY ONE WHO IS UNDEFEATABLE", "All")
-        wait(10)
-        game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("I WILL DESTROY YOU ALL!!", "All")
+        game.Players.LocalPlayer.Character.Humanoid.Health = 0
         wait(0.5)
-        game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("(I LIEK SMELLY FEET)", "All")
+        game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("X_X", "All")     
+    end
+if string.find(chat,"magic") then
+        game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("woah woah whats happening....", "All")
+        wait(2)
+        game.Players.LocalPlayer.Character.Humanoid.HipHeight = 100
+        game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("OMG HELP", "All")
+        wait(5)
+        game.Players.LocalPlayer.Character.Humanoid.HipHeight = 2
+        wait(1)
+        game.Players.LocalPlayer.Character.Humanoid.Health = 0
+        wait(0.5)
+        game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("X_X", "All")     
     end
 end)
