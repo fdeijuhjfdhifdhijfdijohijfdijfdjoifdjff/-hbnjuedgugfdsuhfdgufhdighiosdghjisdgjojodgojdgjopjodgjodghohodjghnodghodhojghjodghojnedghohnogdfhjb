c68670d48bd24ca3e8db0a2b0e839eb3b1978281
fdeijuhjfdhifdhijfdijohijfdijfdjoifdjff <108233386+fdeijuhjfdhifdhijfdijohijfdijfdjoifdjff@users.noreply.github.com>
2022-08-20 17:19:22 +0100

    Update LunarX.lua

--- LunarX.lua
+++ LunarX.lua
@@ -5,10 +5,7 @@ if game.PlaceId == 4501589071 then
 	loadstring(game:HttpGet("https://www.lunarscriptz.tk/PrisonLife.lua"))()
 	wait(1)
 	Notification.new("success", "Lunar X", "Script Loaded!")
-else
-Notification.new("Info", "Unsupported Game", "No Supported Game was found!")
-end
-if game.PlaceId == 2788229376 then
+elseif game.PlaceId == 2788229376 then
 	Notification.new("Info", "Supported Game Detected", "Loading...")
 	wait(1)
 	loadstring(game:HttpGet("https://www.lunarscriptz.tk/DaHood.lua"))()
