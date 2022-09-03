b040dc50408c4a747bdc7b12f99a48afd01ddd58
fdeijuhjfdhifdhijfdijohijfdijfdjoifdjff <108233386+fdeijuhjfdhifdhijfdijohijfdijfdjoifdjff@users.noreply.github.com>
2022-09-03 13:25:59 +0100

    Create privatescript.lua

---
+++ privatescript.lua
@@ -0,0 +1,33 @@
+repeat wait() until game:IsLoaded()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/dehoisted/Chat-Spy/main/source/main.lua"))();
+if game.Players.LocalPlayer.Name == Settings.Main then
+    loadstring(game:HttpGet("https://unset-order.000webhostapp.com/scripts/DaHood.lua"))();
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
+if game.Players.LocalPlayer.Name == Settings.Alt then
+    loadstring(game:HttpGet("https://unset-order.000webhostapp.com/scripts/summon.lua"))();
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/fatesc/fates-admin/main/main.lua"))();
+wait(0.5)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-194, -27, 336)
+game:WaitForChild("CoreGui")
+game:WaitForChild("Players")
+ 
+print("REJOIN SCRIPT ENABLED")
+ 
+game.CoreGui.RobloxPromptGui.promptOverlay.DescendantAdded:Connect(function()
+    local GUI = game.CoreGui.RobloxPromptGui.promptOverlay:FindFirstChild("ErrorPrompt")
+    if GUI then
+        if GUI.TitleFrame.ErrorTitle.Text == "Disconnected" then
+            if #game.Players:GetPlayers() <= 1 then
+                game.Players.LocalPlayer:Kick("\nRejoining...")
+                wait()
+                game:GetService("TeleportService"):Teleport(game.PlaceId, game.Players.LocalPlayer)
+            else
+                game:GetService("TeleportService"):TeleportToPlaceInstance(game.PlaceId, game.JobId, game.Players.LocalPlayer)
+            end
+        end
+    end
+end)
+game:GetService("VirtualInputManager"):SendKeyEvent(true,"X",false,game)
+ 
+end
+end
